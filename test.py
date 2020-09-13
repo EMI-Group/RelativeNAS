350439cbdbaff738ff9fbbea413db9856f733bd3
Almost <tanbox@live.com>
2020-09-13 16:53:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ parser.add_argument('--cutout', action='store_true', default=False, help='use cu
 parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
 parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
 parser.add_argument('--seed', type=int, default=0, help='random seed')
-parser.add_argument('--arch', type=str, default='PairNAS', help='which architecture to use')
+parser.add_argument('--arch', type=str, default='RelativeNAS', help='which architecture to use')
 parser.add_argument('--set', type=str, default="cifar10", help='data set')
 
 args = parser.parse_args()
